1baeeb37649e061e2085b3c612ea49bed3fb93a2
scudetto <marcinlukaszewicz82@gmail.com>
2016-09-02 09:59:36 +0200

    solution

--- OddOccurencesInArray/OddOccurencesInArray/main.cpp
+++ OddOccurencesInArray/OddOccurencesInArray/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int solution(vector<int> &A) {
+    // write your code in C++11 (g++ 4.8.2)
+    int result = 0;
+
+    for(int i=0; i<A.size();i++)
+    {
+        result ^= A[i];
+    }
+
+    return result;
+}
+
+
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+
+    vector<int> myVec = {9,3,9,3,9,7,9};
+
+    cout<<solution(myVec)<<endl;
+
     return 0;
 }
